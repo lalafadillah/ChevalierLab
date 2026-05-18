f3da7367a716e83dac4ec2c983760612bf90b54d
Lala Fadhilah Khairunisa <lalafadillah.696@gmail.com>
2026-05-18 15:49:33 +0700

    kondisi if and switch

---
+++ kondisi.js
@@ -0,0 +1,25 @@
+    const umur = 12;
+
+    // If Else Statement
+    if(umur >= 17) {
+        console.log('Kamu sudah boleh buat KTP');
+    }else if(umur >= 20){
+        console.log('Kamu sudah boleh buat SIM');
+    }else{
+        console.log('Kamu belum boleh buat KTP dan SIM');
+    }
+    // Switch Case Statement
+    switch(umur){
+        case 17:
+            console.log('Kamu sudah boleh buat KTP');
+            break;
+        case 20:
+            console.log('Kamu sudah boleh buat SIM');
+            break;
+        default:
+            console.log('Kamu belum boleh buat KTP dan SIM');
+            break;
+    }
+    // Ternary Operator
+    const status = umur >= 17? 'Kamu sudah boleh buat KTP': 'Kamu belum boleh buat KΤΡ';
+    // output: Kamu sudah boleh buat KTP
