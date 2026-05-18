983d6edd7b12a63d84d0b53e9a776ab25481e36a
Lala Fadhilah Khairunisa <lalafadillah.696@gmail.com>
2026-05-18 16:03:13 +0700

    belajar  perulangan

---
+++ perulangan.js
@@ -0,0 +1,16 @@
+    let ii = 0
+    // for statement
+    for (let i=0; i < 10; i++) {
+        console.log(i);
+    }
+    // while statement
+    while (ii < 10) {
+    console.log(ii);
+    ii++;
+    }
+    // do while statement
+    do {
+    console.log(ii);
+    ii++;
+    } while (ii < 10)
+    // output: 0123456789
