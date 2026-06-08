8e83b56c2fdb37332edb7d9da3397b2150e7c241
Lala Fadhilah Khairunisa <lalafadillah.696@gmail.com>
2026-06-08 20:42:14 +0700

    tipe data

---
+++ week4/tipeData.js
@@ -0,0 +1,23 @@
+// String
+const namaewa = "Rifky Muhammad Prayudhi";
+// Number
+const umur = 20;
+// Boolean
+const isJomblo = true;
+// Array
+const hobi = ["baca buku", "masak", "fotografi"]
+const gataumaunamainapa = [1,2,3,4,5,6,7,8,9,10]
+// Object
+const biodata = {
+nama: "Rifky Muhammad Prayudhi",
+umur: 20,
+isJomblo: true,
+hobi: ["baca buku", "masak", "fotografi"]
+}
+
+console.log(namaewa);
+console.log(umur);
+console.log(isJomblo);
+console.log(hobi);
+console.log(gataumaunamainapa);
+console.log(biodata);
