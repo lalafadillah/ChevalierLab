6c62d9b3f889764eff10ec5c1377863d832668f2
Lala Fadhilah Khairunisa <lalafadillah.696@gmail.com>
2026-06-10 07:14:50 +0700

    Aksesoris DOM

---
+++ week4/getElemenBy.js
@@ -0,0 +1,6 @@
+// getElementById
+const title = document.getElementById("title");
+// getElementsByClassName
+const description = document.getElementsByClassName("description");
+// getElementsByTagliame
+const list = document.getElementsByTagName("li");
