480d957d3285e9c1aa0cdf3debfb3f09a6950330
Lala Fadhilah Khairunisa <lalafadillah.696@gmail.com>
2026-06-23 22:01:17 +0700

    update to do liat

--- week6/src/App.jsx
+++ week6/src/App.jsx
@@ -1,31 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const [todos, setTodos] = useState([
+    
+  ]);
+
+  const tambahTodo = () => {
+    if (input.trim() === "") return;
+
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
+  const hapusTodo = (index) => {
+    const todoBaru = todos.filter((_, i) => i !== index);
+    setTodos(todoBaru);
+  };
 
   return (
-    <>
-      <section>
-        <div id="myDIV" class="header">
-  <h2>My To Do List</h2>
-  <input type="text" id="myInput" placeholder="Title..."></input>
-  <span onclick="newElement()" class="addBtn">Add</span>
-</div>
+    <div className="container">
+      <div className="header">
+        <h1>🎀 My Cute Activity List 🎀</h1>
+        <p>Catat semua kegiatanmu hari ini </p>
 
-<ul id="myUL">
-  <li>Hit the gym</li>
-  <li class="checked">Pay bills</li>
-  <li>Meet George</li>
-  <li>Buy eggs</li>
-  <li>Read a book</li>
-  <li>Organize office</li>
-</ul>
-      </section>
-    </>
-  )
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Tulis kegiatan baru..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") tambahTodo();
+            }}
+          />
+
+          <button onClick={tambahTodo}>
+            ➕ Tambah
+          </button>
+        </div>
+      </div>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <span>{todo}</span>
+
+            <button
+              className="done-btn"
+              onClick={() => hapusTodo(index)}
+            >
+              ✅ Selesai
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
