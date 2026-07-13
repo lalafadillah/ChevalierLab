dfe2f09657deede3835120993c62ac5728941ead
Lala Fadhilah Khairunisa <lalafadillah.696@gmail.com>
2026-07-13 21:58:53 +0700

    header web

--- week6/src/components/Header.jsx
+++ week6/src/components/Header.jsx
@@ -1,8 +1,17 @@
-function Header(props){
-    return(
-        <header>
-            <h2>{props.nama}</h2>
-        </header>
-    );
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <h2>Chevalier Laboratory</h2>
+      <nav>
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <hr />
+    </header>
+  );
 }
+
 export default Header;
