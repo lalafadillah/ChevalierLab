30ced6d556ce5ae5780245c5aeb1bc0b6bdbe76a
Lala Fadhilah Khairunisa <lalafadillah.696@gmail.com>
2026-07-14 21:44:22 +0700

    update home

--- week6/src/pages/Home.jsx
+++ week6/src/pages/Home.jsx
@@ -1,5 +1,15 @@
-function Home(){
-    return <h1>Home</h1>;
+function Home() {
+  return (
+    <>
+      <p>
+      Welcome to my first React website.
+      </p>
+      
+      <p>
+      This website was built using React JS and React Router.
+      </p>
+    </>
+  );
 }
 
 export default Home;
