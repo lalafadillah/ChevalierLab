0cd23b97bcfecf06c986507fb446d502640581c1
Lala Fadhilah Khairunisa <lalafadillah.696@gmail.com>
2026-07-27 21:03:18 +0700

    about

---
+++ week7/src/pages/About.jsx
@@ -0,0 +1,9 @@
+import {Link} from "react-router";
+export default function About() {
+  return (
+    <>
+    <h1>ini adalah halaman About</h1>
+    <Link to="/Home"> redirect ke halaman Home</Link>
+    </>
+  )
+}
